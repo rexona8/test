eda9ad17fc806e804c8656053fa3e4fe5d3f68d6
Nikita Vasilchenko <rexona8@yandex.ru>
2023-03-27 13:03:08 +0300

    added new task decision

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(System.Console.ReadLine());
+
+for (int i = 2; i < number; i+=2)
+{
+    System.Console.WriteLine(i);
+}
