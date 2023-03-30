a06d4b3b01e3ebee89fac754b2943eeb1c3c7596
Nikita Vasilchenko <rexona8@yandex.ru>
2023-03-30 13:52:21 +0300

    added new comment for task000

--- task000/Program.cs
+++ task000/Program.cs
@@ -7,7 +7,7 @@
 // 2. Напишите программу, которая выводит случайное число из отрезка [10, 99] и 
 // показывает наибольшую цифру числа.
 
-int Maxnumber(int number)
+int Maxnumber(int num)
 {
     int num1 = num / 10;
     int num2 = num % 10;
---
+++ task0000/Program.cs
@@ -0,0 +1,49 @@
+﻿// 3. Напишите программу, которая будет принимать на вход два числа и выводить, 
+// является ли второе число кратным первому. Если число 2 не кратно числу 1, 
+// то программа выводит остаток от деление.
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+// int ReadInt(string message)
+// {
+//     System.Console.WriteLine($"{message}");
+//     int value = Convert.ToInt32(System.Console.ReadLine());
+//     return value;
+// }
+
+// int num1 = ReadInt("input 1st number");
+// int num2 = ReadInt("input 2nd number");
+// int remains = num1 % num2;
+// if (remains == 0)
+// {
+//     System.Console.WriteLine("Второе число кратно первому");
+// }
+// else
+// {
+//     System.Console.WriteLine($"Не кратно, остаток {remains}");
+// }
+
+
+// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно 
+// одновременно 7 и 23.
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+int ReadInt(string message)
+{
+    System.Console.WriteLine($"{message}");
+    int value = Convert.ToInt32(System.Console.ReadLine());
+    return value;
+}
+
+int num = ReadInt("input number");
+
+if (num % 7 == 0 && num % 23 == 0)
+{
+    System.Console.WriteLine("the number is a multiple of 7 and 23");
+}
+else
+{
+    System.Console.WriteLine("not multiple");
+}
