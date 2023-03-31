bd50260915d8007674baa009d594c3cb8cc427a3
Nikita Vasilchenko <rexona8@yandex.ru>
2023-03-31 12:47:14 +0300

    added new decision for task000000

--- task000000/Program.cs
+++ task000000/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая выводит третью цифру заданного числа или 
+// сообщает, что третьей цифры нет. Не использовать строки для расчета.
+
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+
+int ThirdNumber(int x, int y)
+{
+int result = 0;
+    if (y < 3)
+    {
+        Console.Write("Нет. ");
+    }
+    else
+    {
+        int z = 1;
+        for (int i = y; i > 3; i--)
+        {
+            z = z * 10;
+        }
+        result = (x / z) % 10;
+    }
+return result;
+}
+
+int number = ReadInt("Введите число: ");
+int count = number.ToString().Length;
+System.Console.WriteLine($"Третья цифра в {number} будет {ThirdNumber(number, count)}");
