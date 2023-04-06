c23143fa79233156d2034ef01de123192d670298
Nikita Vasilchenko <rexona8@yandex.ru>
2023-04-06 19:16:33 +0300

    added new desicion for task00000000

--- task00000000/Program.cs
+++ task00000000/Program.cs
@@ -1,16 +1,46 @@
-﻿// Задача 3: Напишите программу, которая принимает на вход число (N) и выдаёт 
-// таблицу кубов чисел от 1 до N.
-// 3 -> 1, 8, 27. 
-// 5 -> 1, 8, 27, 64, 125
+﻿// Задача 3: Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] CreateArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+int Count1(int[] array, int number)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number)
+        {
+            count++;
+        }
+    }
+    return count;
+}
 
 int ReadInt(string message)
 {
-    System.Console.Write($"{message}");
+    System.Console.Write($"{message} = ");
     return System.Convert.ToInt32(Console.ReadLine());
 }
 
-int number = ReadInt("Введите число: ");
-for (int i = 0; i < number; i++)
-{
-    System.Console.WriteLine(Math.Pow(i, 3));
-}
+int number = ReadInt("Введите число:");
+int[] writeArray = CreateArray();
+PrintArray(writeArray);
+System.Console.WriteLine($"Количество заданного числа в массиве = {Count1(writeArray, number)}");
